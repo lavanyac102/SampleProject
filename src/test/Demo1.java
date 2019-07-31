3c4445220449a1107a5834dd04853bf213aaa550
lavanyac102 <52200833+lavanyac102@users.noreply.github.com>
2019-07-31 15:47:35 +0530

    Update Demo1.java

--- src/test/Demo1.java
+++ src/test/Demo1.java
@@ -3,8 +3,9 @@ package test;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		System.out.println("inside main");
-System.out.println("in GIT Sample1 branch");
+		
+	System.out.println("in GIT Sample2 branch");
+		System.out.println("merging it to sample1");
 	}
 
 }
